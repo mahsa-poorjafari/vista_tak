98a19b853a3ce6453f7f54ef842251d003cd237d
mahsa <m_mw7462@yahoo.com>
2014-12-30 10:32:36 +0330

    start new project, clone form pack_b

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PackB::Application.config.secret_key_base = '0137ef090211af6f1b664d1e40710c44e00d64cf98d5fcf92dcec39150acd239806d69c42c1e0426d7463d8a5bd8af901ccd3d9ede36718208b80d616cd7ce1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PackB::Application.config.session_store :cookie_store, key: '_pack_b_session'
