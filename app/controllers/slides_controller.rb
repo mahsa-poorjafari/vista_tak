f115c03eab71a6a1d61ae478034f347081559027
mahsa <m_mw7462@yahoo.com>
2014-12-31 12:25:18 +0330

    add logout to admin menu

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -69,6 +69,6 @@ class SlidesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def slide_params
-      params.require(:slide).permit(:description, :description_fa)
+      params.require(:slide).permit(:description, :description_fa, :image)
     end
 end
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -2,4 +2,12 @@
 class Slide < ActiveRecord::Base
   has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  
+  def description    
+    if I18n.locale == :en
+      self.read_attribute("description")
+    else
+      self.read_attribute("description_fa")
+    end    
+  end
 end
