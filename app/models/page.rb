ce974da57c59d9a9341328ee0717a683c3af798e
mahsa <m_mw7462@yahoo.com>
2015-01-06 10:36:48 +0330

    change color of p , h1-h5 to black

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 class Page < ActiveRecord::Base
-
+  extend FriendlyId  
+  friendly_id :title_fa 
   def title    
     if I18n.locale == :en
       self.read_attribute("title_en")
